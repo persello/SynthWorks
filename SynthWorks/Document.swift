f4c1a8e27fb1914f037a99f2a7bdcb55246d802b
Riccardo Persello <riccardo.persello+i@icloud.com>
2021-07-26 16:19:44 +0200

    Initial Commit

---
+++ SynthWorks/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  SynthWorks
+//
+//  Created by Riccardo Persello on 26/07/21.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
